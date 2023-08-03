bb4ed1bccba29dbdbeaa0eb81699348e36c2deaa
Jay Parmar <jay.parmar@biztechcs.lan>
2023-08-03 18:07:20 +0530

    Remove console log

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  console.log(`${process.env.API_URL}/posts`);
-
   const res = await fetch(`${process.env.API_URL}/posts`, {
     headers: {
       'Content-Type': 'application/json',
